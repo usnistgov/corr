7acb8406dd78d6c5889544a6473c9aa2c5314852
Yannick Congo <faical.congo@nist.gov>
2016-12-19 14:14:09 -0500

    Fixing custom rendering.

--- corr-view/frontend/js/render.js
+++ corr-view/frontend/js/render.js
@@ -10,5 +10,42 @@ var renderer = {
                 failed();
             }
         });
+    },
+    application: function(object, ownership, layout){
+        $.ajax({
+            url : "/xml/"+xml_location,
+            type: 'GET',
+            success: function(result,status,xhr) {
+                succeed(xhr, params);
+            },
+            error: function(xhr,status,error) {
+                failed();
+            }
+        });
+    },
+    project: function(object, ownership, layout){
+        console.log("Hello from project.");
+        $.ajax({
+            url : "/xml/"+xml_location,
+            type: 'GET',
+            success: function(result,status,xhr) {
+                succeed(xhr, params);
+            },
+            error: function(xhr,status,error) {
+                failed();
+            }
+        });
+    },
+    record: function(object, ownership, layout){
+        $.ajax({
+            url : "/xml/"+xml_location,
+            type: 'GET',
+            success: function(result,status,xhr) {
+                succeed(xhr, params);
+            },
+            error: function(xhr,status,error) {
+                failed();
+            }
+        });
     }
 }
