19e7c06c027b98681404571f4de85f9bc5ac3882
Yannick Congo <faical.congo@nist.gov>
2016-12-28 11:38:25 -0500

    Issue with parent null for record extend and env stats not being propagated to the project.

--- corr-cloud/cloud/views/env_cloud.py
+++ corr-cloud/cloud/views/env_cloud.py
@@ -93,7 +93,6 @@ def env_create(hash_session, record_id):
                         if application_id:
                             application = ApplicationModel.objects.with_id(application_id)
                             if application:
-                                application
                                 application.records = application.records + 1
                                 application.save()
                                 if str(current_user.id) not in application.users:
@@ -107,6 +106,7 @@ def env_create(hash_session, record_id):
                         env.system = system
                         env.save()
                         record.project.history.append(str(env.id))
+                        record.project.save()
                         record.environment = env
                         record.save()
                         return cloud_response(201, 'Environment successfully created.', "The environment was created.")
