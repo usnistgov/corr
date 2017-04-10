cd3c4062ef4d3eb6ee1b463cebebce72e96000d9
Yannick Congo <faical.congo@nist.gov>
2017-04-09 22:38:09 -0400

    Stats sizes.

--- corr-cloud/cloud/views/user_cloud.py
+++ corr-cloud/cloud/views/user_cloud.py
@@ -275,14 +275,15 @@ def user_dashboard():
                 project_dash = {"name":"{0}~{1}".format(project.owner.email, project.name), "records":{"January":{"number":0, "size":0}, "February":{"number":0, "size":0}, "March":{"number":0, "size":0}, "April":{"number":0, "size":0}, "May":{"number":0, "size":0}, "June":{"number":0, "size":0}, "July":{"number":0, "size":0}, "August":{"number":0, "size":0}, "September":{"number":0, "size":0}, "October":{"number":0, "size":0}, "November":{"number":0, "size":0}, "December":{"number":0, "size":0}}}
                 records = RecordModel.objects(project=project)
                 dashboard["records_total"] += len(records)
-                dashboard["environments_total"] += len(project.history)
                 for record in records:
                     environment = record.environment
                     size = 0
                     try:
-                        size = environment.bundle.size
+                        size = environment.bundle["size"]
                     except:
                         size = 0
+
+                    dashboard["environments_total"] += size
 
                     month = str(record.created_at).split("-")[1]
                     if month == "01":
