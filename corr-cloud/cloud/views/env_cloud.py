75b71807097b4e75b4e1c0b9940fc13b90c58e44
Faical Yannick Palingwende Congo <faical.congo@nist.gov>
2017-05-01 14:35:41 -0400

    Fixing the version location info replacing : with | instead as a separator.

--- corr-cloud/cloud/views/env_cloud.py
+++ corr-cloud/cloud/views/env_cloud.py
@@ -189,7 +189,7 @@ def env_next(project_id):
                         version = VersionModel(created_at=str(datetime.datetime.utcnow()))
                         system = data.get("version", "unknown")
                         vc_location = data.get("version-location")
-                        vc_baselines = vc_location.split(":")
+                        vc_baselines = vc_location.split("|")
                         if len(vc_baselines) > 0:
                             version.baseline = vc_baselines[0]
                         if len(vc_baselines) > 1:
