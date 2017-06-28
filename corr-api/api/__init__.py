46c714244eed7d8c861ba1ab723fa1948acd55d0
Faical Yannick Palingwende Congo <faical.congo@nist.gov>
2017-06-28 11:59:12 -0400

    Content as is.

--- corr-api/api/__init__.py
+++ corr-api/api/__init__.py
@@ -36,7 +36,7 @@ API_URL = '/corr/api/v{0}'.format(API_VERSION)
 
 
 def secure_content(content):
-    security = storage_manager.is_safe(bytes(str(content), 'UTF-8'))
+    security = storage_manager.is_safe(content)
     if not security[0]:
         return fk.Response(security[1], status.HTTP_406_NOT_ACCEPTABLE)
 
